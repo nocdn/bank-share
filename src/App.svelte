6a964eae898119bfbc931ad213371a4bfa0b620f
Bartek <bartek.bak@protonmail.com>
2024-10-06 18:48:20 +0100

    Reset everything again

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,47 @@
 <script>
-    import "./app.css";
-    import Detail from "./Detail.svelte";
-    import { Button } from "$lib/components/ui/button";
-    import { Toaster } from "$lib/components/ui/sonner";
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-    <Detail name="John Doe" sortCode="12-34-56" accountNumber="78008093"
-    ></Detail>
-    <Detail name="Bartosz Bak" sortCode="98-19-77" accountNumber="98562028"
-    ></Detail>
-    <Button on:click={() => toast("Hello world")}>Show toast</Button>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
 </main>
 
 <style>
-    main {
-        padding: 3rem;
-
-        display: flex;
-        flex-direction: column;
-        gap: 1.5rem;
-    }
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
 </style>
